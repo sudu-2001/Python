8d48662bb65f2ae818d51c8857295d1d23d8dc8b
sudarshan <kosudarshan15@gmail.com>
2024-05-20 08:11:31 +0530

    success

---
+++ class.py
@@ -0,0 +1,11 @@
+class dog:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    def bark(self):
+        print("wolf")
+
+mydog=dog("buddy",9)
+print(mydog.name)
+print(mydog.age)
