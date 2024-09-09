a42131be81d879ebbf0d0936565e67ebc5460303
sudarshan <kosudarshan15@gmail.com>
2024-09-09 12:11:23 +0530

    h

---
+++ dict/clear.py
@@ -0,0 +1,9 @@
+dict={
+    "name":"alice",
+    "id":1,
+    "reg":21
+}
+
+dict.clear()
+
+print(dict)
---
+++ dict/pop.py
@@ -0,0 +1,9 @@
+dict={
+    "name":"alice",
+    "id":1,
+    "reg":21
+}
+
+dict.pop("id")
+
+print(dict)
